f6abd008c4e162b903e98d57c399d54dd3dc445a
DH_Park <102042000+ManRaccoon@users.noreply.github.com>
2025-04-23 20:16:33 +0900

    Create 11657.py

---
+++ 250423/DH/11657번: 타임머신/11657.py
@@ -0,0 +1,31 @@
+import sys
+
+input = sys.stdin.readline
+INF = int(1e9)
+
+n, m = map(int, input().split())
+edges = []
+
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    edges.append((a, b, c))
+
+distance = [INF] * (n + 1)
+distance[1] = 0  # 시작 도시
+
+def bellman_ford():
+    for i in range(n - 1):
+        for u, v, w in edges:
+            if distance[u] != INF and distance[v] > distance[u] + w:
+                distance[v] = distance[u] + w
+
+    for u, v, w in edges:
+        if distance[u] != INF and distance[v] > distance[u] + w:
+            return False
+    return True
+
+if bellman_ford():
+    for i in range(2, n + 1):
+        print(distance[i] if distance[i] != INF else -1)
+else:
+    print(-1)
