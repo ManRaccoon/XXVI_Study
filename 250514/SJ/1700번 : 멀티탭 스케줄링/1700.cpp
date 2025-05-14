10f2260a1830189213852475f1e31ee6cd351dde
Sejun An <s3037444@kookmin.ac.kr>
2025-05-14 21:01:05 +0900

    Update 1700.cpp

--- 250514/SJ/1700번 : 멀티탭 스케줄링/1700.cpp
+++ 250514/SJ/1700번 : 멀티탭 스케줄링/1700.cpp
@@ -12,22 +12,22 @@ int main() {
         cin >> schedule[i];
     }
 
-    vector<int> plugged; // 현재 꽂혀 있는 기기
+    vector<int> plugged; 
     int answer = 0;
 
     for (int i = 0; i < K; ++i) {
         int now = schedule[i];
 
-        // 이미 꽂혀 있는 경우
+     
         if (find(plugged.begin(), plugged.end(), now) != plugged.end()) continue;
 
-        // 멀티탭에 자리가 있는 경우
+   
         if (plugged.size() < N) {
             plugged.push_back(now);
             continue;
         }
 
-        // 뽑아야 하는 경우
+
         int farthest = -1;
         int idx_to_remove = -1;
 
@@ -40,11 +40,11 @@ int main() {
                 }
             }
 
-            if (next_use == -1) { // 다시 안 쓰이는 기기
+            if (next_use == -1) { 
                 idx_to_remove = j;
                 break;
             }
-            else if (next_use > farthest) { // 가장 나중에 쓰이는 기기
+            else if (next_use > farthest) { 
                 farthest = next_use;
                 idx_to_remove = j;
             }
